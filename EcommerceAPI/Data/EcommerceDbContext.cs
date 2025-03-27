2b477cf79d8cac98223715aa714e49e5f73ac2a6
pbhushan7 <bvphulpagar24@gmail.com>
2025-03-27 17:29:51 +0530

    First Commit

---
+++ EcommerceAPI/Data/EcommerceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EcommerceAPI.Data
+{
+    public class EcommerceDbContext : DbContext
+    {
+        public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- EcommerceAPI/obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
+++ EcommerceAPI/obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e381451972aef155f21c5c5fc45aed516586e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
