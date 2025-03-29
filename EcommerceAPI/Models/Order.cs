5fa7d04c5535e10032fc564bdc46e23978de6dab
pbhushan7 <bvphulpagar24@gmail.com>
2025-03-29 19:18:54 +0530

    Order Model

---
+++ EcommerceAPI/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace EcommerceAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
--- EcommerceAPI/obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
+++ EcommerceAPI/obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b477cf79d8cac98223715aa714e49e5f73ac2a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a10b7bb9008e6b0c81797b1aa5704425db5b455")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
